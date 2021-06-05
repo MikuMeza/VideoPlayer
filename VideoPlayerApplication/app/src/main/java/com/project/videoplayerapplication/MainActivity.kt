7e90a8991faa7bf99959ff6426587c56c0721d1d
Mikha Johnson <nnatasha1027@gmail.com>
2021-06-06 01:37:21 +0530

    second change

--- VideoPlayerApplication/app/src/main/java/com/project/videoplayerapplication/MainActivity.kt
+++ VideoPlayerApplication/app/src/main/java/com/project/videoplayerapplication/MainActivity.kt
@@ -1,137 +1,71 @@
 package com.project.videoplayerapplication
 
-import android.content.Context
+
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.android.exoplayer2.*
-import com.google.android.exoplayer2.source.MediaSource
-import com.google.android.exoplayer2.source.ProgressiveMediaSource
-import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.upstream.DataSource
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.ExoPlayerFactory
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
+import com.google.android.exoplayer2.extractor.ExtractorsFactory
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.hls.HlsMediaSource
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.trackselection.TrackSelector
+import com.google.android.exoplayer2.upstream.BandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import com.project.videoplayerapplication.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity(), Player.Listener {
+class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var simpleExoplayer: SimpleExoPlayer
+    private lateinit var exoPlayer: SimpleExoPlayer
+//    private val videoURL = "https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
+    val videoURL =
+        "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4";
 
-    private lateinit var videoView: PlayerView
-    private lateinit var exoPlayer: ExoPlayer
-    private lateinit var context: Context
-
-    private var playbackPosition: Long = 0
-    private val mp4Url = "https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-
-    //    private val dashUrl = "https://storage.googleapis.com/wvmedia/clear/vp9/tears/tears_uhd.mpd"
-    private val urlList = listOf(mp4Url to "default")
-
-    private val dataSourceFactory: DataSource.Factory by lazy {
-        DefaultDataSourceFactory(this, "exoplayer-sample")
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-//        setContentView(R.layout.activity_main)
+        try {
+//            val trackSelector = DefaultTrackSelector()
+//            val exoPlayer = SimpleExoPlayer.newSimpleInstance(baseContext,trackSelector)
+//            exoPlayerView?.player = exoPlayer
+
+
+
+            //bandwisthmeter is used for getting default bandwidth
+            val bandwidthMeter = DefaultBandwidthMeter()
+            // track selector is used to navigate between video using a default seekbar.
+            val trackSelector =
+                DefaultTrackSelector()
+            //we are ading our track selector to exoplayer.
+            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
+            // we are parsing a video url and parsing its video uri.
+            val videouri = Uri.parse(videoURL)
+            // we are creating a variable for datasource factory and setting its user agent as 'exoplayer_view'
+            val dataSourceFactory = DefaultHttpDataSourceFactory("exoplayer_video")
+            // we are creating a variable for extractor factory and setting it to default extractor factory.
+            val extractorsFactory: ExtractorsFactory = DefaultExtractorsFactory()
+            //we are creating a media source with above variables and passing our event handler as null,
+//            val mediaSource = HlsMediaSource(videouri, dataSourceFactory, null, null)
+            val mediaSource = ExtractorMediaSource(videouri, dataSourceFactory,extractorsFactory, null, null)
+            //inside our exoplayer view we are setting our player
+            binding.exoplayerView.player = exoPlayer
+            //we are preparing our exoplayer with media source.
+            exoPlayer.prepare(mediaSource)
+            //we are setting our exoplayer when it is ready.
+            exoPlayer.playWhenReady = true
+        } catch (e: Exception) {
+            // below line is used for handling our errors.
+            Log.e("TAG", "Error : $e")
+        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        initializePlayer()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        releasePlayer()
-    }
-
-    private fun initializePlayer() {
-        simpleExoplayer = SimpleExoPlayer.Builder(this).build()
-//        val randomUrl = urlList.random()
-        preparePlayer(mp4Url, " ")
-        binding.exoplayerView.player = simpleExoplayer
-        simpleExoplayer.seekTo(playbackPosition)
-        simpleExoplayer.playWhenReady = true
-        simpleExoplayer.addListener(this)
-    }
-
-    private fun buildMediaSource(uri: Uri, type: String): MediaSource {
-//        return if (type == "dash") {
-//           return DashMediaSource.Factory(dataSourceFactory)
-//                    .createMediaSource(uri)
-//        } else {
-        return ProgressiveMediaSource.Factory(dataSourceFactory)
-                .createMediaSource(MediaItem.fromUri(uri))
-//        }
-    }
-
-    private fun preparePlayer(videoUrl: String, type: String) {
-//        val uri = Uri.parse(videoUrl)
-//        val mediaSource = buildMediaSource(uri, type)
-//        simpleExoplayer.setMediaSource(mediaSource)
-//        simpleExoplayer.prepare()
-
-        // Build the media item.
-        // Build the media item.
-        val mediaItem: MediaItem = MediaItem.fromUri(videoUrl)
-// Set the media item to be played.
-// Set the media item to be played.
-        simpleExoplayer.setMediaItem(mediaItem)
-// Prepare the player.
-// Prepare the player.
-        simpleExoplayer.prepare()
-// Start the playback.
-// Start the playback.
-        simpleExoplayer.play()
-    }
-
-    private fun releasePlayer() {
-        playbackPosition = simpleExoplayer.currentPosition
-        simpleExoplayer.release()
-    }
-
-    override fun onPlayerError(error: ExoPlaybackException) {
-        // handle error
-    }
-
-    override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
-        if (playbackState == Player.STATE_BUFFERING)
-            binding.progressBar.visibility = View.VISIBLE
-        else if (playbackState == Player.STATE_READY || playbackState == Player.STATE_ENDED)
-            binding.progressBar.visibility = View.INVISIBLE
-    }
-
-//    override fun onIsPlayingChanged(isPlaying: Boolean) {
-//        if(isPlaying)
-//            binding.progressBar.visibility = View.INVISIBLE
-//        else
-//            binding.progressBar.visibility = View.VISIBLE
-//    }
 }
-
-    //Setting Up Exoplayer
-//
-
-//}  private fun SetupPlayer() {
-//        val simpleExoPlayer: SimpleExoPlayer
-//        // Create a data source factory.
-//        dataSourceFactory = DefaultHttpDataSourceFactory(Util.getUserAgent(this, applicationInfo.loadLabel(packageManager).toString()))
-//        // Passing Load Control
-//        var loadControl = DefaultLoadControl.Builder()
-//                .setBufferDurationsMs(25000, 50000, 1500, 2000).createDefaultLoadControl()
-//        @ExtensionRendererMode val extensionRendererMode = DefaultRenderersFactory.EXTENSION_RENDERER_MODE_PREFER
-//        var renderersFactory = DefaultRenderersFactory(this).setExtensionRendererMode(extensionRendererMode)
-//
-//// Create a progressive media source pointing to a stream uri.
-//        var mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
-//                .createMediaSource(Uri.parse(url_to_stream))
-//        // Create a player instance.
-//        simpleExoPlayer = SimpleExoPlayer.Builder(this, renderersFactory).setLoadControl(loadControl).build()
-//        // Prepare the player with the media source.
-//        simpleExoPlayer.prepare(mediaSource, true, true)
-//    }
