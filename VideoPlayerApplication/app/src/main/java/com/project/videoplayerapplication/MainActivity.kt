2979b2473fa1d63bf5a7aaca2af4bbbe39cc2146
Mikha Johnson <nnatasha1027@gmail.com>
2021-06-06 19:25:22 +0530

    New changes

--- VideoPlayerApplication/app/src/main/java/com/project/videoplayerapplication/MainActivity.kt
+++ VideoPlayerApplication/app/src/main/java/com/project/videoplayerapplication/MainActivity.kt
@@ -11,61 +11,97 @@ import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory
 import com.google.android.exoplayer2.extractor.ExtractorsFactory
 import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.hls.HlsMediaSource
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.trackselection.TrackSelector
+import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.BandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
-import com.project.videoplayerapplication.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.util.Util
 
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var exoPlayer: SimpleExoPlayer
-//    private val videoURL = "https://bitmovin-a.akamaihd.net/content/sintel/hls/playlist.m3u8"
-    val videoURL =
-        "https://media.geeksforgeeks.org/wp-content/uploads/20201217163353/Screenrecorder-2020-12-17-16-32-03-350.mp4";
+    private var mPlayer: SimpleExoPlayer? = null
+    private lateinit var playerView: PlayerView
+    private var playWhenReady = true
+    private var currentWindow = 0
+    private var playbackPosition: Long = 0
+    private val hlsUrl =
+        "https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8"
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        try {
-//            val trackSelector = DefaultTrackSelector()
-//            val exoPlayer = SimpleExoPlayer.newSimpleInstance(baseContext,trackSelector)
-//            exoPlayerView?.player = exoPlayer
+        playerView = findViewById(R.id.playerView)
 
 
+    }
 
-            //bandwisthmeter is used for getting default bandwidth
-            val bandwidthMeter = DefaultBandwidthMeter()
-            // track selector is used to navigate between video using a default seekbar.
-            val trackSelector =
-                DefaultTrackSelector()
-            //we are ading our track selector to exoplayer.
-            exoPlayer = ExoPlayerFactory.newSimpleInstance(this, trackSelector)
-            // we are parsing a video url and parsing its video uri.
-            val videouri = Uri.parse(videoURL)
-            // we are creating a variable for datasource factory and setting its user agent as 'exoplayer_view'
-            val dataSourceFactory = DefaultHttpDataSourceFactory("exoplayer_video")
-            // we are creating a variable for extractor factory and setting it to default extractor factory.
-            val extractorsFactory: ExtractorsFactory = DefaultExtractorsFactory()
-            //we are creating a media source with above variables and passing our event handler as null,
-//            val mediaSource = HlsMediaSource(videouri, dataSourceFactory, null, null)
-            val mediaSource = ExtractorMediaSource(videouri, dataSourceFactory,extractorsFactory, null, null)
-            //inside our exoplayer view we are setting our player
-            binding.exoplayerView.player = exoPlayer
-            //we are preparing our exoplayer with media source.
-            exoPlayer.prepare(mediaSource)
-            //we are setting our exoplayer when it is ready.
-            exoPlayer.playWhenReady = true
-        } catch (e: Exception) {
-            // below line is used for handling our errors.
-            Log.e("TAG", "Error : $e")
+    private fun initPlayer() {
+        mPlayer = SimpleExoPlayer.Builder(this).build()
+        // Bind the player to the view.
+        playerView.player = mPlayer
+        mPlayer!!.playWhenReady = true
+        mPlayer!!.seekTo(playbackPosition)
+        mPlayer!!.prepare(buildMediaSource(), false, false)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT >= 24) {
+            initPlayer()
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT < 24 || mPlayer == null) {
+            initPlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT < 24) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT >= 24) {
+            releasePlayer()
+        }
+    }
+
+
+    private fun releasePlayer() {
+        if (mPlayer == null) {
+            return
+        }
+        playWhenReady = mPlayer!!.playWhenReady
+        playbackPosition = mPlayer!!.currentPosition
+        currentWindow = mPlayer!!.currentWindowIndex
+        mPlayer!!.release()
+        mPlayer = null
+    }
+
+    private fun buildMediaSource(): MediaSource {
+        val userAgent =
+            Util.getUserAgent(playerView.context, playerView.context.getString(R.string.app_name))
+
+        val dataSourceFactory = DefaultHttpDataSourceFactory("exo-player")
+        val hlsMediaSource =
+            HlsMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(hlsUrl))
+
+        return hlsMediaSource
     }
 
 }
